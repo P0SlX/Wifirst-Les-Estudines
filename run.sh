ed723a2d02367bfc236ce43156c86b63734a0dc2
P0SlX <florian.savoure@gmail.com>
2022-11-15 21:37:09 +0100

    new cookie "Portal-AuthToken"

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-COOKIE=$(wget -qO- https://wireless.wifirst.net:8099/index.txt)
-EMAIL="exemple@email.com"
-BODYDATA="{\"fragment_id\":2683,\"box_token\":\"$COOKIE\",\"guest_user\":{\"email\":\"$EMAIL\",\"cgu\":true}}"
+
 UA="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36"
 
+wget -qO- >> /dev/null http://wifirst.net/ \
+  --load-cookies /tmp/cookies.txt \
+  --save-cookies /tmp/cookies.txt \
+  --keep-session-cookies \
+  --header "User-Agent: $UA" \
+  --no-check-certificate
+
+COOKIE=$(wget -qO- https://wireless.wifirst.net/index.txt)
+EMAIL="exemple@email.com"
+BODYDATA="{\"fragment_id\":2683,\"box_token\":\"$COOKIE\",\"guest_user\":{\"email\":\"$EMAIL\",\"cgu\":true}}"
 
 CREDS=$(wget -qO- \
   --method POST \
@@ -17,9 +25,9 @@ CREDS=$(wget -qO- \
 
 USERNAME=$(echo $CREDS | awk 'BEGIN{RS=":"}{print $1}' | cut -d '"' -f2 | tail -3 | head -1)
 PASSWORD=$(echo $CREDS | awk 'BEGIN{RS=":"}{print $1}' | cut -d '"' -f2 | tail -2 | head -1)
-BODYDATA2="username=$USERNAME&password=$PASSWORD&success_url=https%3A%2F%2Fwww.myresidhome.com&error_url=https%3A%2F%2Fportal-front.wifirst.net%2Fconnect-error&update_session=1"
+BODYDATA2="username=$USERNAME&password=$PASSWORD&success_url=https%3A%2F%2Fwww.myresidhome.com&error_url=https%3A%2F%2Fportal-front.wifirst.net%2Fconnect-error&update_session=0"
 
-wget -q0- >> /dev/null \
+wget -qO- >> /dev/null \
   --method POST \
   --load-cookies /tmp/cookies.txt \
   --save-cookies /tmp/cookies.txt \
@@ -27,4 +35,4 @@ wget -q0- >> /dev/null \
   --header 'Content-Type: application/x-www-form-urlencoded' \
   --header "User-Agent: $UA" \
   --body-data $BODYDATA2 \
-  'https://wireless.wifirst.net:8090/goform/HtmlLoginRequest'
+  'https://wireless.wifirst.net/goform/HtmlLoginRequest'
